70e751e3a38ca051dd416fce064b82bb43cc1e61
RemiLisciandra <rlisciandra@gmail.com>
2023-04-17 09:58:17 +0200

    REST API complete

--- data/dataGenerator.js
+++ data/dataGenerator.js
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker';
+import {faker} from '@faker-js/faker';
 
 const generateUser = (id) => {
     return {
@@ -7,17 +7,19 @@ const generateUser = (id) => {
         username: faker.internet.userName(),
         email: faker.internet.email(),
         password: faker.internet.password(),
-        age: faker.datatype.number({ min: 10, max: 100 }),
+        age: faker.datatype.number({min: 10, max: 100}),
         picture: faker.image.avatar(),
         cars: [faker.vehicle.vehicle(), faker.vehicle.vehicle()],
         created: faker.date.between('2023-01-01T00:00:00.000Z', '2024-01-01T00:00:00.000Z'),
     };
 };
 
-const users = [];
+const generateUsersList = (count) => {
+    let usersList = [];
+    for (let i = 1; i <= count; i++) {
+        usersList.push(generateUser(i));
+    }
+    return usersList;
+};
 
-for (let i = 1; i <= 1000; i++) {
-    users.push(generateUser(i));
-}
-
-export default users;
+export { generateUsersList };
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import users from './data/data.js';
 import express from 'express';
 import morgan from 'morgan';
 import favicon from 'serve-favicon';
 import bodyParser from "body-parser";
+import { getUsers, addUser, updateUser, patchUser, deleteUser } from './data/userMethods.js';
 
 const server = express();
 const port = 3000;
@@ -10,25 +10,47 @@ const port = 3000;
 // Middlewares
 server.use(favicon('favicon.ico')).use(morgan('dev')).use(bodyParser.json());
 
-// GET Methods
+// GET methods
 server.get('/api/users/:id', (req, res) => {
         const id = parseInt(req.params.id);
-        const user = users.find(user => user.id === id);
-        res.json(user);
+        const user = getUsers().find(user => user.id === id);
+        return res.json(user);
     }
 );
-
 server.get('/api/users', (req, res) => {
-        res.json(users);
-    }
-);
+    return res.json(getUsers());
+});
 
-// POST Methods
+// POST method
 server.post('/api/users', (req, res) => {
-    const id = users.length + 1;
+    const id = getUsers().length + 1;
     const userCreated = {...req.body, ...{id: id, created: new Date()}};
-    users.push(userCreated);
-    res.json(userCreated);
+    addUser(userCreated);
+    return res.json(userCreated);
+});
+
+// PUT method
+server.put('/api/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const userUpdated = { ...req.body, id: id }
+    updateUser(id, userUpdated);
+    return res.json(userUpdated);
+});
+
+// PATCH method
+server.patch('/api/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const updatedUser = req.body;
+    const userPatched = patchUser(id, updatedUser);
+    return res.json(userPatched);
+});
+
+// DELETE method
+server.delete('/api/users/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const userDeleted = getUsers().find(user => user.id === id);
+    deleteUser(id);
+    return res.json(userDeleted);
 });
 
 // Listen api
